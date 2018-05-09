ecbf6d5c0c675ec22aba932fc62d3453a4bd0f5d
Fabian Schmick <fabian.schmick@myboom.de>
2018-05-09 16:13:14 +0200

    Test all routes.

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,16 +3,62 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * PHPUnit's data providers allow to execute the same tests repeated times
+     * using a different set of data each time.
+     * See http://symfony.com/doc/current/cookbook/form/unit_testing.html#testing-against-different-sets-of-data.
+     *
+     * @dataProvider getPublicUrls
+     */
+    public function testPublicUrls($url)
     {
         $client = static::createClient();
+        $client->followRedirects();
+        $client->request('GET', $url);
 
-        $crawler = $client->request('GET', '/');
+        $this->assertSame(
+            Response::HTTP_OK,
+            $client->getResponse()->getStatusCode(),
+            sprintf('The %s public URL loads correctly.', $url)
+        );
+    }
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    /**
+     * The application contains a lot of secure URLs which shouldn't be
+     * publicly accessible. This tests ensures that whenever a user tries to
+     * access one of those pages, a redirection to the login form is performed.
+     *
+     * @dataProvider getSecureUrls
+     */
+    public function testSecureUrls($url)
+    {
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame(
+            'http://localhost/login',
+            $response->getTargetUrl(),
+            sprintf('The %s secure URL redirects to the login form.', $url)
+        );
+    }
+
+    public function getPublicUrls()
+    {
+        yield ['/form/1'];
+        yield ['/value/1'];
+    }
+
+    public function getSecureUrls()
+    {
+        yield ['/admin/'];
+        yield ['/admin/builder/'];
+        yield ['/admin/builder/1/'];
+        yield ['/admin/builder/1/values'];
     }
 }
